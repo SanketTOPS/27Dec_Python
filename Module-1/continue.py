f2801325dcb6b80f66ba484e6bfb653a8e6fb215
SanketTops <sanket.tops@gmail.com>
2025-01-10 17:50:26 +0530

    loops

---
+++ Module-1/continue.py
@@ -0,0 +1,8 @@
+"""for i in range(1,11):
+    if i==6:
+        continue #SKIP
+    print(i)"""
+
+for i in range(1,11):
+    if i>5:
+        print(i)
---
+++ Module-1/pattern1.py
@@ -0,0 +1,5 @@
+for i in range(1,6): #ROW
+    for j in range(i): #COLUMN
+        print("*",end=" ")
+    print(" ") #line break
+
