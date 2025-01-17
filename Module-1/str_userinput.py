37277cc9f102f140918c1eedf7f668e001b25c6e
SanketTops <sanket.tops@gmail.com>
2025-01-17 17:32:11 +0530

    String

---
+++ Module-1/str_userinput.py
@@ -0,0 +1,6 @@
+mystr=input("Enter any string:")
+
+if mystr.startswith("This") or mystr.startswith("THIS"):
+    print("Done!")
+else:
+    print("Error!")
---
+++ Module-1/string.py
@@ -0,0 +1,32 @@
+mystr="this is python!"
+"""print(mystr)
+print(mystr[0])
+print(mystr[-1])
+print(mystr[0:6]) #slice - range
+print(mystr[5:])
+print(mystr[:4])"""
+
+#print(len(mystr)) #length 
+ 
+# --------------------------------- #
+print(mystr)
+#print(mystr.strip())
+#print(mystr.lower())
+#print(mystr.upper())
+#print(mystr.replace('This','That'))
+#print(mystr.replace('T','Y'))
+#print(mystr.split())
+#print(mystr.capitalize())
+#print(mystr.casefold())
+#print(mystr.count('I'))
+#print(mystr.count('python'))
+#print(mystr.endswith('89'))
+#print(mystr.startswith('THIS'))
+#print(mystr.find('T'))
+#print(mystr.index('T'))
+#print(mystr.isalpha())
+#print(mystr.isdigit())
+#print(mystr.isalnum())
+#print(mystr.islower())
+#print(mystr.isupper())
+print(mystr.title())
