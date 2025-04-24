d620d727d4112331b36d61dc2e72df56ddbc2cf5
SanketTops <sanket.tops@gmail.com>
2025-04-24 16:19:19 +0530

    Full Auth

--- Django Project/AuthProject/myapp/views.py
+++ Django Project/AuthProject/myapp/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import *
+from django.contrib.auth import logout
 
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        unm = request.POST.get("username")
+        pas = request.POST.get("password")
+
+        user = userSignup.objects.filter(username=unm, password=pas)
+        if user:  # TRUE
+            print("Login Successfully!")
+            request.session["user"] = unm  # Generate session
+            return redirect("home")
+        else:
+            print("Error!Login faild...")
     return render(request, "index.html")
 
 
@@ -13,6 +25,17 @@ def signup(request):
         if form.is_valid():
             form.save()
             print("Signup Successfully!")
+            return redirect("/")
         else:
             print(form.errors)
     return render(request, "signup.html")
+
+
+def home(request):
+    user = request.session.get("user")
+    return render(request, "home.html", {"user": user})
+
+
+def userlogout(request):
+    logout(request)
+    return redirect("/")
