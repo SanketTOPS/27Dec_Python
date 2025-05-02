a2368e3665220c81b83ad079664761b14b9a4d0a
SanketTops <sanket.tops@gmail.com>
2025-05-02 16:27:58 +0530

    Templates Extends

--- Django Project/FInalProject/noteapp/urls.py
+++ Django Project/FInalProject/noteapp/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from noteapp import views
 
 
 urlpatterns = [
-   path("",views.index),
+    path("", views.index),
+    path("notes/", views.notes, name="notes"),
+    path("profile/", views.profile, name="profile"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
 ]
--- Django Project/FInalProject/noteapp/views.py
+++ Django Project/FInalProject/noteapp/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request, "index.html")
+
+
+def notes(request):
+    return render(request, "notes.html")
+
+
+def profile(request):
+    return render(request, "profile.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
