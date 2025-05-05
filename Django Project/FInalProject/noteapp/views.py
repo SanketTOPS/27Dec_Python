7dd2896c0ded8b090f76b8979ed759b3b82b8ad3
SanketTops <sanket.tops@gmail.com>
2025-05-05 16:27:02 +0530

    Email Sending

--- Django Project/FInalProject/noteapp/views.py
+++ Django Project/FInalProject/noteapp/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import *
 from .models import *
+from django.core.mail import send_mail
+import random
+from FInalProject import settings
 
 
 # Create your views here.
@@ -29,11 +32,30 @@ def login(request):
 
 
 def signup(request):
+    msg = ""
     if request.method == "POST":
         form = UsersignupForm(request.POST, request.FILES)
+        username = ""
         if form.is_valid():
-            form.save()
-            print("Signup successfully!")
+            username = form.cleaned_data.get("username")
+            try:
+                Usersignup.objects.get(username=username)
+                print("Username is already exists!")
+                msg = "Username is already exists!"
+            except Usersignup.DoesNotExist:
+                form.save()
+                print("Signup successfully!")
+                msg = "Signup Successfully!"
+
+                # Email Sending Code
+                otp = random.randint(11111, 99999)
+                send_mail(
+                    subject="Your OTP Verification",
+                    message=f"Dear User!\n\nThank you for registration with us!\nYour one time password is {otp}\n\nThanks & Regards!\nNotesApp\nTOPS Technologies\n+9724799469 | sanket.tops@gmail.com",
+                    from_email=settings.EMAIL_HOST_USER,
+                    recipient_list=[request.POST["username"]],
+                )
+                return redirect("login")
         else:
             print(form.errors)
-    return render(request, "signup.html")
+    return render(request, "signup.html", {"msg": msg})
